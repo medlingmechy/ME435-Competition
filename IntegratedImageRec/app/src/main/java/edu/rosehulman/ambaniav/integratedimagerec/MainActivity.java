7efd7e8c6b821086afeac39e7f3d130f248a29d2
medlingmechy <37915348+medlingmechy@users.noreply.github.com>
2018-05-01 14:38:55 -0400

    UnRunnable Script

--- IntegratedImageRec/app/src/main/java/edu/rosehulman/ambaniav/integratedimagerec/MainActivity.java
+++ IntegratedImageRec/app/src/main/java/edu/rosehulman/ambaniav/integratedimagerec/MainActivity.java
@@ -11,7 +11,7 @@ import android.view.MenuItem;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends GolfBallDeliveryActivity {
 
     private ViewFlipper mViewFlipper;
     @Override
@@ -27,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        mViewFlipper.showNext();
         return true;
     }
 
@@ -40,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (id == R.id.action_next) {
             Toast.makeText(this,"You Pressesd NEXT",Toast.LENGTH_SHORT).show();
+            mViewFlipper.showNext();
 
             return true;
         }
