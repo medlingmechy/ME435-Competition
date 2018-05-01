3229bd65830547bf70100c7ddbf0cc0200a8ec1d
medlingmechy <37915348+medlingmechy@users.noreply.github.com>
2018-05-01 14:01:48 -0400

    Prepare 3 placeholderviews

--- IntegratedImageRec/app/src/main/java/edu/rosehulman/ambaniav/integratedimagerec/MainActivity.java
+++ IntegratedImageRec/app/src/main/java/edu/rosehulman/ambaniav/integratedimagerec/MainActivity.java
@@ -8,30 +8,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewFlipper mViewFlipper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mViewFlipper = findViewById(R.id.View_Flipper);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        mViewFlipper.showNext();
         return true;
     }
 
@@ -42,8 +38,9 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_next) {
+            Toast.makeText(this,"You Pressesd NEXT",Toast.LENGTH_SHORT).show();
+
             return true;
         }
 
