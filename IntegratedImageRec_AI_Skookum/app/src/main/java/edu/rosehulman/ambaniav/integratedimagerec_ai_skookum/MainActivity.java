1ebc5a4460fde045c4bc2aed86cd669e5542f70b
medlingmechy <37915348+medlingmechy@users.noreply.github.com>
2018-05-06 22:04:09 -0400

    Waitingto Test

--- IntegratedImageRec_AI_Skookum/app/src/main/java/edu/rosehulman/ambaniav/integratedimagerec_ai_skookum/MainActivity.java
+++ IntegratedImageRec_AI_Skookum/app/src/main/java/edu/rosehulman/ambaniav/integratedimagerec_ai_skookum/MainActivity.java
@@ -1,49 +1,38 @@
 package edu.rosehulman.ambaniav.integratedimagerec_ai_skookum;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends GolfBallDeliveryActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Note: The superclass does this, if I do it too. That's bad!
+        //setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (item.getItemId() == R.id.action_next) {
+//      Toast.makeText(this, "You pressed Next", Toast.LENGTH_SHORT).show();
+            mViewFlipper.showNext();
             return true;
         }
 
